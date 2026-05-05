28c3ed33b38cc1564fc3c66b8799ec3bf20680f2
Hanashiko <hlichisper@gmail.com>
2026-05-05 17:53:49 +0300

    added vm-from-iso using

---
+++ using/vm-from-is/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "0.85.1"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint = var.proxmox_endpoint
+  api_token = var.proxmox_api_token
+  insecure = true
+}
+
+module "ubuntu_install_vm" {
+  source = "../../modules/vm-from-iso"
+
+  proxmox_node = "pve-2"
+  vm_name = "ubuntu-2404-install"
+  vm_id = 8999
+
+  iso_source = "datastore"
+  iso_file_name = "ubuntu-24.04.2-live-server-amd64.iso"
+  iso_datastore = "local-brtfs"
+
+  cpu_cores = 1
+  memory_mb = 1024
+  disk_size = 10
+  bios = "ovmf"
+  machine_type = "i440fx"
+
+  started = false
+}
