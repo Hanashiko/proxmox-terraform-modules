c089476e0908e54407e429fcc412f108c3c21ba3
Hanashiko <hlichisper@gmail.com>
2026-05-06 10:57:49 +0300

    added in vm from cloud image

--- using/vm-from-cloud-image/main.tf
+++ using/vm-from-cloud-image/main.tf
@@ -32,3 +32,46 @@ module "web_01" {
   make_template = false
   started = true
 }
+
+# одразу в темплейт для подальшого клонування
+module "ubuntu_2404_template" {
+  source = "../../modules/vm-from-cloud-image"
+
+  proxmox_node = "pve-2"
+  vm_name      = "ubuntu-2404-cloud"
+  vm_id        = 9001
+
+  image_url      = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
+  image_datastore = "local-btrfs"
+  disk_datastore  = "local-btrfs"
+
+  cpu_cores = 2
+  memory_mb = 2048
+  disk_size = 20
+
+  cloud_init_user = "ubuntu"
+  ssh_public_keys = [file("~/.ssh/id_ed25519.pub")]
+
+  # Одразу темплейт — не стартує, cloud-init не виконається
+  # (виконається при першому старті клону)
+  make_template = true
+}
+
+# Клонуємо з cloud-темплейта — cloud-init спрацює при першому старті клону
+module "app_server" {
+  source = "../../modules/vm-from-cloud-image"
+
+  proxmox_node = "pve-2"
+  vm_name      = "app-01"
+  template_id  = module.ubuntu_2404_template.vm_id
+
+  cpu_cores = 4
+  memory_mb = 8192
+  disk_size = 50
+
+  ipv4_address    = "192.168.10.20/24"
+  ipv4_gateway    = "192.168.10.1"
+  ssh_public_keys = [file("~/.ssh/id_ed25519.pub")]
+
+  depends_on = [module.ubuntu_2404_template]
+}
